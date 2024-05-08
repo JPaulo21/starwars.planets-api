111f6fea48940b6e6013737eb0e8223a6e989c9d
João Paulo Lira de Almeida <joao.almeida@mv.com.br>
2024-05-08 14:35:50 -0300

    First Commit

---
+++ src/test/java/com/jp/tests/api/starwars/planets/PlanetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jp.tests.api.starwars.planets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
