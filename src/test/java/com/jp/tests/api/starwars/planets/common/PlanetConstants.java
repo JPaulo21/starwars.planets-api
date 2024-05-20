996488704f63ae697803da694795cc28fcebd486
João Paulo Lira de Almeida <joao.almeida@mv.com.br>
2024-05-20 14:27:11 -0300

    test: colocado campo climate do Planet e adicionado invalid_planet

--- src/test/java/com/jp/tests/api/starwars/planets/common/PlanetConstants.java
+++ src/test/java/com/jp/tests/api/starwars/planets/common/PlanetConstants.java
@@ -4,6 +4,7 @@ import com.jp.tests.api.starwars.planets.domain.Planet;
 
 public class PlanetConstants {
 
-    public static final Planet  PLANET = new Planet("name","climante","terrain");
+    public static final Planet PLANET = new Planet("name","climate","terrain");
+    public static final Planet INVALID_PLANET = new Planet("","","");
 
 }
