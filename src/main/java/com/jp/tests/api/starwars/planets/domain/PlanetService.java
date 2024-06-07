db3ca3d85c4e4b46fca868271037eaeb859a4400
João Paulo Lira de Almeida <joao.almeida@mv.com.br>
2024-06-07 17:18:28 -0300

    feat: Implementado busca de Planet com uma query dinâmica com QueryByExample

--- src/main/java/com/jp/tests/api/starwars/planets/domain/PlanetService.java
+++ src/main/java/com/jp/tests/api/starwars/planets/domain/PlanetService.java
@@ -1,10 +1,14 @@
 package com.jp.tests.api.starwars.planets.domain;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class PlanetService {
@@ -21,5 +25,10 @@ public class PlanetService {
 
     public Optional<Planet> getByName(String name){
         return planetRepository.findByName(name);
+    }
+
+    public List<Planet> list(String terrain, String climate) {
+        Example<Planet> query = QueryBuilder.makeQuery(Planet.builder().terrain(terrain).climate(climate).build());
+        return planetRepository.findAll(query);
     }
 }
