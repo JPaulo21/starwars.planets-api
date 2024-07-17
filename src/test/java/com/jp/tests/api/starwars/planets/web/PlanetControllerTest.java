738d448b4e04446bf11c2a709349a70441560776
João Paulo Lira de Almeida <joao.almeida@mv.com.br>
2024-07-17 13:49:17 -0300

    test: Implementado classe para teste do controller e criação de planet com dados válidos.

---
+++ src/test/java/com/jp/tests/api/starwars/planets/web/PlanetControllerTest.java
@@ -0,0 +1,41 @@
+package com.jp.tests.api.starwars.planets.web;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static com.jp.tests.api.starwars.planets.common.PlanetConstants.PLANET;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jp.tests.api.starwars.planets.domain.PlanetService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(PlanetController.class)
+public class PlanetControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private PlanetService planetService;
+
+    @Test
+    public void createPlanet_WithValidData_ReturnsCreated() throws Exception {
+        when(planetService.create(PLANET)).thenReturn(PLANET);
+
+        mockMvc.perform(post("/planets")
+                        .content(objectMapper.writeValueAsString(PLANET))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$").value(PLANET));
+    }
+
+}
