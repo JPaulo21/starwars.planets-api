11e56502b8c63328d2a5298447d6604f7b99c022
João Paulo Lira de Almeida <joao.almeida@mv.com.br>
2024-07-18 14:26:27 -0300

    feat: validando dados da entity ao receber Planet no corpo da requisição.

--- src/main/java/com/jp/tests/api/starwars/planets/web/PlanetController.java
+++ src/main/java/com/jp/tests/api/starwars/planets/web/PlanetController.java
@@ -2,6 +2,7 @@ package com.jp.tests.api.starwars.planets.web;
 
 import com.jp.tests.api.starwars.planets.domain.Planet;
 import com.jp.tests.api.starwars.planets.domain.PlanetService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,7 +18,7 @@ public class PlanetController {
     private final PlanetService planetService;
 
     @PostMapping
-    public ResponseEntity<Planet> create(@RequestBody Planet planetData){
+    public ResponseEntity<Planet> create(@RequestBody @Valid Planet planetData){
         Planet planet = planetService.create(planetData);
         return ResponseEntity.status(HttpStatus.CREATED).body(planet);
     }
