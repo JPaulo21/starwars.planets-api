4284f4b9169ab236f9025255a625aa22c2e537e4
João Paulo Lira de Almeida <joao.almeida@mv.com.br>
2024-07-18 14:39:12 -0300

    test: Verificando resposta da api para nome já existente do planet

--- src/test/java/com/jp/tests/api/starwars/planets/web/PlanetControllerTest.java
+++ src/test/java/com/jp/tests/api/starwars/planets/web/PlanetControllerTest.java
@@ -1,5 +1,6 @@
 package com.jp.tests.api.starwars.planets.web;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -13,6 +14,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -55,4 +57,12 @@ public class PlanetControllerTest {
                 .andExpect(status().isUnprocessableEntity());
     }
 
+    @Test
+    public void createPlanet_WithExistingName_ReturnsConflict() throws Exception {
+        when(planetService.create(any())).thenThrow(DataIntegrityViolationException.class);
+
+        mockMvc.perform(post("/planets").content(objectMapper.writeValueAsString(PLANET))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isConflict());
+    }
 }
