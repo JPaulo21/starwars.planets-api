e73acfe84f74aea219523b5e08784f86bdc58c75
João Paulo Lira de Almeida <joao.almeida@mv.com.br>
2024-07-19 08:06:20 -0300

    feat: Tratando erro de integridade dos dados do bean validator

--- src/main/java/com/jp/tests/api/starwars/planets/web/exception/GeneralExceptionHandler.java
+++ src/main/java/com/jp/tests/api/starwars/planets/web/exception/GeneralExceptionHandler.java
@@ -1,13 +1,14 @@
 package com.jp.tests.api.starwars.planets.web.exception;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.method.annotation.HandlerMethodValidationException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
@@ -18,4 +19,11 @@ public class GeneralExceptionHandler extends ResponseEntityExceptionHandler {
                                                                   HttpStatusCode status, WebRequest request) {
         return super.handleMethodArgumentNotValid(ex, headers, HttpStatus.UNPROCESSABLE_ENTITY, request);
     }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    private ResponseEntity<Object> handlerConflict(DataIntegrityViolationException ex) {
+        return ResponseEntity.status(HttpStatus.CONFLICT)
+                .body(ex.getMessage());
+    }
+
 }
