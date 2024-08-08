562f268c3a46e7545b978abacb52ceff5a9baa6b
João Paulo Lira de Almeida <joao.almeida@mv.com.br>
2024-08-08 14:52:21 -0300

    test: Criada lista de planets

--- src/test/java/com/jp/tests/api/starwars/planets/common/PlanetConstants.java
+++ src/test/java/com/jp/tests/api/starwars/planets/common/PlanetConstants.java
@@ -2,9 +2,15 @@ package com.jp.tests.api.starwars.planets.common;
 
 import com.jp.tests.api.starwars.planets.domain.Planet;
 
+import java.util.List;
+
 public class PlanetConstants {
 
     public static final Planet PLANET = new Planet("name","climate","terrain");
     public static final Planet INVALID_PLANET = new Planet("","","");
 
+    public static final Planet TATOOINE = new Planet(1L, "Tatooine", "arid", "desert");
+    public static final Planet ALDERAAN = new Planet(2L, "Alderaan", "temperate", "grasslands, mountains");
+    public static final Planet YAVINIV = new Planet(2L, "Yavin IV", "temperate, tropical", "jungle, rainforest");
+    public static final List<Planet> PLANETS = List.of(TATOOINE, ALDERAAN, YAVINIV);
 }
