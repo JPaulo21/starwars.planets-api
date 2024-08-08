5efca2505e537508db84a8bd59d5ca82af584e53
João Paulo Lira de Almeida <joao.almeida@mv.com.br>
2024-08-08 14:54:17 -0300

    cleanup: Retirado QueryByExample pois já é implementado no JpaRepository

--- src/main/java/com/jp/tests/api/starwars/planets/domain/PlanetRepository.java
+++ src/main/java/com/jp/tests/api/starwars/planets/domain/PlanetRepository.java
@@ -2,14 +2,13 @@ package com.jp.tests.api.starwars.planets.domain;
 
 import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.QueryByExampleExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface PlanetRepository extends JpaRepository<Planet, Long>, QueryByExampleExecutor<Planet> {
+public interface PlanetRepository extends JpaRepository<Planet, Long> {
     Optional<Planet> findByName(String name);
 
     @Override
